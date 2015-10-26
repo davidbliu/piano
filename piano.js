0e4f260d570b5779a014f02793cba2e0d29fa6e9
David Liu <davidbliu@gmail.com>
2015-10-25 21:47:38 -0700

    demo

--- piano.js
+++ piano.js
@@ -1,19 +1,30 @@
 PIANO_DIR = './piano-sounds/';
 SUFFIX = '.mp3';
+DRUM_DIR = './drum-sounds/';
 
 //piano key map
 keyMap = {};
-keyMap['a'] = 'A0';
-keyMap['s'] = 'A1';
-keyMap['d'] = 'A2';
-keyMap['f'] = 'A3';
-keyMap['g'] = 'A4';
-keyMap['h'] = 'A5';
-keyMap['j'] = 'A6';
-keyMap['k'] = 'A7';
-keyMap['l'] = 'B6';
-keyMap[';'] = 'B5';
-
+keyMap['a'] = 'A3';
+keyMap['s'] = 'B3';
+keyMap['d'] = 'C4';
+keyMap['f'] = 'D4';
+keyMap['g'] = 'E4';
+keyMap['h'] = 'F4';
+keyMap['j'] = 'G4';
+keyMap['k'] = 'A4';
+keyMap['l'] = 'B4';
+keyMap[';'] = 'C5';
+//drum key map
+keyMap['q'] = '1';
+keyMap['w'] = '2';
+keyMap['e'] = '3';
+keyMap['r'] = '4';
+keyMap['t'] = '5';
+keyMap['y'] = '6';
+keyMap['u'] = '7';
+keyMap['i'] = '8';
+keyMap['o'] = '9';
+keyMap['p'] = '10';
 function drawKeyboard(){
   //draw keyboard using keys in the mapping
   keys = Object.keys(keyMap);
@@ -22,13 +33,14 @@ function drawKeyboard(){
     div = document.createElement('div');
     $(div).addClass('key');
     $(div).attr('id',keyMap[key]);
-    $(div).text(keyMap[key]);
+    $(div).text(key);
     $('#piano-div').append(div);
   }
 }
 function animateKey(key){
   $('#'+key).fadeOut(100).fadeIn(100);
 }
+
 function playSound(key){
   var sound = new Audio(PIANO_DIR+key.toString()+SUFFIX);
   sound.play();
